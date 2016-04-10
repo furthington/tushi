4b400966083170c85a6695e025138fb1c92252a0
fuer <contact@fu-er.com>
2016-04-10 15:29:32 -0700

    Add initial files

---
+++ Assets/scripts/Scene/LoadLevelProxy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scene
+{
+  public class LoadLevelProxy : MonoBehaviour
+  {
+    public void LoadLevel(Object scene)
+    {
+      PumpNotifications();
+      SceneManager.LoadScene(scene.name);
+    }
+
+    public void LoadLevel(int index)
+    {
+      PumpNotifications();
+      SceneManager.LoadScene(index);
+    }
+
+    public void LoadLevel(string name)
+    {
+      PumpNotifications();
+      SceneManager.LoadScene(name);
+    }
+
+    private void PumpNotifications()
+    { /*Notification.Pool.PumpAll();*/ }
+  }
+}
