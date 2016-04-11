5b2dc19de2465b1218eec5cf537e22b810d9e742
jeaye <contact@jeaye.com>
2016-04-11 00:00:40 -0700

    Correct line endings

--- Assets/scripts/Board/PieceTray.cs
+++ Assets/scripts/Board/PieceTray.cs
@@ -1,1 +1,25 @@
-using UnityEngine;using System.Collections.Generic;namespace Board{  public class PieceTray : MonoBehaviour  {    public List<GameObject> Prefabs;    public void Regenerate()    {      foreach (Transform child in transform)      { Destroy(child.gameObject); }      for(int i = 0; i < 3; ++i)      { Instantiate(Prefabs[Random.Range(0, Prefabs.Count)]).transform.SetParent(transform, false); }    }    public void Rotate()    {      foreach (Transform child in transform)      { child.Rotate(new Vector3(0, 0, -60)); }    }  }}
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Board
+{
+  public class PieceTray : MonoBehaviour
+  {
+    public List<GameObject> Prefabs;
+
+    public void Regenerate()
+    {
+      foreach (Transform child in transform)
+      { Destroy(child.gameObject); }
+
+      for(int i = 0; i < 3; ++i)
+      { Instantiate(Prefabs[Random.Range(0, Prefabs.Count)]).transform.SetParent(transform, false); }
+    }
+
+    public void Rotate()
+    {
+      foreach (Transform child in transform)
+      { child.Rotate(new Vector3(0, 0, -60)); }
+    }
+  }
+}
--- Assets/scripts/Board/Tile.cs
+++ Assets/scripts/Board/Tile.cs
@@ -1,1 +1,22 @@
-using UnityEngine;using UnityEngine.UI;using System.Collections.Generic;using UnityEngine.EventSystems;namespace Board{  public class Tile : MonoBehaviour, IPointerClickHandler  {    public List<Tile> SurroundingTiles;    private Image img;    public void Start()    { img = GetComponent<Image>(); }    public void OnPointerClick(PointerEventData data)    {      Image color_holder = GameObject.FindGameObjectWithTag("color").GetComponent<Image>();      img.color = color_holder.color;    }  }}
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+namespace Board
+{
+  public class Tile : MonoBehaviour, IPointerClickHandler
+  {
+    public List<Tile> SurroundingTiles;
+    private Image img;
+
+    public void Start()
+    { img = GetComponent<Image>(); }
+
+    public void OnPointerClick(PointerEventData data)
+    {
+      Image color_holder = GameObject.FindGameObjectWithTag("color").GetComponent<Image>();
+      img.color = color_holder.color;
+    }
+  }
+}
