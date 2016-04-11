648e9be5aa483480cee941a28e02b2e23b095d53
jeaye <contact@jeaye.com>
2016-04-11 00:06:13 -0700

    Clean up board state by using Tile

--- Assets/tushi/board.clj
+++ Assets/tushi/board.clj
@@ -13,15 +13,5 @@
 (defn start-hook
   [this]
   (let [board (object-named "board")
-        ; TODO: Add a component to each hex and just use this fn?
-        groups (get-components-in-children
-                 board
-                 UnityEngine.UI.HorizontalLayoutGroup)
-        all-children (loop [remaining groups
-                            all '()]
-                       (if (empty? remaining)
-                         all
-                         (recur (rest remaining)
-                                (concat all (get-children
-                                              (first remaining))))))]
+        all-children (get-components-in-children board Board.Tile)]
     (Debug/Log (count all-children))))
