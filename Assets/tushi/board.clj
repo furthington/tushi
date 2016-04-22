037ddc255e646aa701c15f0a3b5df7ac55afdbde
jeaye <contact@jeaye.com>
2016-04-21 23:38:09 -0700

    Add initial board save code

--- Assets/tushi/board.clj
+++ Assets/tushi/board.clj
@@ -1,17 +1,31 @@
 (ns tushi.board
   (:use arcadia.core
         tushi.interop)
-  (:import [UnityEngine Debug]))
+  (:import [UnityEngine Application Debug]
+           ArcadiaState))
 
 (def long-width 10)
 (def short-width 9)
 (def height 10)
+(def save-file (str Application/persistentDataPath "/save"))
 
-; TODO: Load/save state
 ; TODO: Find neighbors
+
+(defn save!
+  [this]
+  ; TODO: Only save colors
+  (spit save-file (state this)))
+
+(defn load!
+  [this]
+  ; TODO: Verify file exists
+  (state! this (read-string (slurp save-file))))
 
 (defn start-hook
   [this]
   (let [board (object-named "board")
-        all-children (get-components-in-children board Board.Tile)]
-    (Debug/Log (count all-children))))
+        children (get-components-in-children board Board.Tile)]
+    (Debug/Log (count children))
+    (swap-state! this #(assoc % :children children))
+    (save! this)
+    (load! this)))
