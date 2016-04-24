149b5fe1d20e485f24e362d106812afc9a3e1041
jeaye <contact@jeaye.com>
2016-04-23 21:52:14 -0700

    Refactor neighbor code out

---
+++ Assets/scripts/neighbor.clj
@@ -0,0 +1,90 @@
+(ns tushi.neighbor
+  (:use arcadia.core
+        tushi.interop)
+  (:import [UnityEngine Application Debug]
+           ArcadiaState))
+
+(defn at-position
+  [rows x y]
+  (nth (nth rows y) x))
+
+(defn bottom-half?
+  [face-length y]
+  (>= y face-length))
+
+(defn top-half?
+  [face-length y]
+  (< y (- face-length 1)))
+
+(defn row-width
+  [face-length y]
+  (if (bottom-half? face-length y)
+    (+ face-length (- (- (* 2 face-length) 2) y)) ; Mirrored
+    (+ face-length y)))
+
+(defn top-left
+  [rows face-length x y]
+  (if (bottom-half? face-length y)
+    (at-position rows x (- y 1))
+    (when (and (> y 0) (> x 0))
+      (at-position rows (- x 1) (- y 1)))))
+
+(defn top-right
+  [rows face-length x y]
+  (if (bottom-half? face-length y)
+    (at-position rows (+ 1 x) (- y 1))
+    (when (and (> y 0)
+               (< x (- (row-width face-length y) 1)))
+      (at-position rows x (- y 1)))))
+
+(defn right
+  [rows face-length x y]
+  (if (< x (- (row-width face-length y) 1))
+    (at-position rows (+ 1 x) y)))
+
+(defn left
+  [rows face-length x y]
+  (if (> x 0)
+    (at-position rows (- x 1) y)))
+
+(defn bottom-right
+  [rows face-length x y]
+  (if (top-half? face-length y)
+    (at-position rows (+ 1 x) (+ 1 y))
+    (when (and (< y (- (* 2 face-length) 2))
+               (< x (- (row-width face-length y) 1)))
+      (at-position rows x (+ 1 y)))))
+
+(defn bottom-left
+  [rows face-length x y]
+  (if (top-half? face-length y)
+    (at-position rows x (+ 1 y))
+    (when (and (< y (- (* 2 face-length) 2))
+               (> x 0))
+      (at-position rows (- x 1) (+ 1 y)))))
+
+(defn meet
+  [rows face-length elem]
+  (let [pos (:position elem)
+        x (:x pos)
+        y (:y pos)
+        l (left rows face-length x y)
+        tl (top-left rows face-length x y)
+        tr (top-right rows face-length x y)
+        r (right rows face-length x y)
+        br (bottom-right rows face-length x y)
+        bl (bottom-left rows face-length x y)]
+    (assoc elem
+           :left l
+           :top-left tl
+           :top-right tr
+           :right r
+           :bottom-right br
+           :bottom-left bl)))
+
+(defn introduce
+  [rows face-length]
+  (into []
+        (map #(into []
+                    (map (partial meet rows face-length) %))
+             rows)))
--- Assets/tushi/board.clj
+++ Assets/tushi/board.clj
@@ -1,6 +1,7 @@
 (ns tushi.board
   (:use arcadia.core
         tushi.interop)
+  (:require [tushi.neighbor :as neighbor])
   (:import [UnityEngine Application Debug]
            ArcadiaState))
 
@@ -16,13 +17,6 @@
   [this]
   ; TODO: Verify file exists
   (state! this (read-string (slurp save-file))))
-
-; Find neighbors
-;   given face length
-;     find rows
-;   with each tile and its index
-;     calculate its row
-;     use row info to find its neighbors
 
 (defn hex-rows
   [children face-length]
@@ -51,97 +45,10 @@
                  (not top-half?)
                  top-half?))))))
 
-(defn at-position
-  [rows x y]
-  (nth (nth rows y) x))
-
-(defn bottom-half?
-  [face-length y]
-  (>= y face-length))
-
-(defn top-half?
-  [face-length y]
-  (< y (- face-length 1)))
-
-(defn row-width
-  [face-length y]
-  (if (bottom-half? face-length y)
-    (+ face-length (- (- (* 2 face-length) 2) y)) ; Mirrored
-    (+ face-length y)))
-
-(defn top-left
-  [rows face-length x y]
-  (if (bottom-half? face-length y)
-    (at-position rows x (- y 1))
-    (when (and (> y 0) (> x 0)) ; Inside top left edge
-      (at-position rows (- x 1) (- y 1)))))
-
-(defn top-right
-  [rows face-length x y]
-  (if (bottom-half? face-length y)
-    (at-position rows (+ 1 x) (- y 1))
-    (when (and (> y 0)
-               (< x (- (row-width face-length y) 1))) ; Inside top right edge
-      (at-position rows x (- y 1)))))
-
-(defn right
-  [rows face-length x y]
-  (if (< x (- (row-width face-length y) 1))
-    (at-position rows (+ 1 x) y)))
-
-(defn left
-  [rows face-length x y]
-  (if (> x 0)
-    (at-position rows (- x 1) y)))
-
-(defn bottom-right
-  [rows face-length x y]
-  (if (top-half? face-length y)
-    (at-position rows (+ 1 x) (+ 1 y))
-    (when (and (< y (- (* 2 face-length) 2))
-               (< x (- (row-width face-length y) 1)))
-      (at-position rows x (+ 1 y)))))
-
-(defn bottom-left
-  [rows face-length x y]
-  (if (top-half? face-length y)
-    (at-position rows x (+ 1 y))
-    (when (and (< y (- (* 2 face-length) 2))
-               (> x 0))
-      (at-position rows (- x 1) (+ 1 y)))))
-
-(defn bind-neighbor
-  [rows face-length elem]
-  (let [pos (:position elem)
-        x (:x pos)
-        y (:y pos)
-        l (left rows face-length x y)
-        tl (top-left rows face-length x y)
-        tr (top-right rows face-length x y)
-        r (right rows face-length x y)
-        br (bottom-right rows face-length x y)
-        bl (bottom-left rows face-length x y)]
-    (assoc elem
-           :left l
-           :top-left tl
-           :top-right tr
-           :right r
-           :bottom-right br
-           :bottom-left bl)))
-
-(defn bind-neighbors
-  [rows face-length]
-  (into []
-        (map #(into []
-                    (map (partial bind-neighbor rows face-length) %))
-             rows)))
-
 (defn start-hook
   [this]
   (let [board (object-named "board")
         children (get-components-in-children board Board.Tile)]
-    (Debug/Log (count children))
-    (swap-state! this #(assoc % :children children))
-
-    (let [rows (hex-rows children face-length)]
-      )))
+    (let [rows (hex-rows children face-length)
+          neighbored (neighbor/introduce rows face-length)]
+      (swap-state! this #(assoc % :children neighbored)))))
