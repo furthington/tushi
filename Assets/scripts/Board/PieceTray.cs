238c9712566809330461ee3e43b161207e7330eb
fuer <contact@fu-er.com>
2016-04-28 12:58:44 -0700

    Refined rice generation

--- Assets/scripts/Board/PieceTray.cs
+++ Assets/scripts/Board/PieceTray.cs
@@ -8,19 +8,45 @@ namespace Board
     public List<GameObject> PrefabsLowProb;
     public List<GameObject> PrefabsHighProb;
 
+    public List<GameObject> PrefabsRiceLowProb;
+    public List<GameObject> PrefabsRiceHighProb;
+
+    private const float rice_prob_min = 0.1f;
+    private float rice_prob = 0.0f;
+
+    private void Start()
+    { rice_prob = rice_prob_min; }
+
     public void UsePiece(int i)
     {
       Destroy(transform.GetChild(i).gameObject);
-
-      GameObject new_child;
-      if(Random.Range(0.0f, 1.0f) <= 0.1f)
-      { new_child = Instantiate(PrefabsLowProb[Random.Range(0, PrefabsLowProb.Count)]); }
-      else
-      { new_child = Instantiate(PrefabsHighProb[Random.Range(0, PrefabsHighProb.Count)]); }
-
+      GameObject new_child = GeneratePiece();
       new_child.transform.SetParent(transform, false);
       new_child.transform.SetSiblingIndex(i);
     }
+
+    private GameObject GeneratePiece()
+    {
+      /* Check high or low prob */
+      float prob = Random.Range(0.0f, 1.0f);
+
+      /* Check if we should generate rice */
+      if (Random.Range(0.0f, 1.0f) <= rice_prob)
+      {
+        /* Rice probability resets */
+        rice_prob = rice_prob_min;
+        return (prob <= 0.1f) ? RandomFrom(PrefabsRiceLowProb) : RandomFrom(PrefabsRiceHighProb);
+      }
+      else
+      {
+        /* Rice probability goes up */
+        rice_prob *= 1.5f;
+        return (prob <= 0.1f) ? RandomFrom(PrefabsLowProb) : RandomFrom(PrefabsHighProb);
+      }
+    }
+
+    private GameObject RandomFrom(List<GameObject> prefabs)
+    { return Instantiate(prefabs[Random.Range(0, prefabs.Count)]); }
 
     public void Rotate()
     {
