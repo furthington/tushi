f26f255802c7a96ce712a0b0815d105a99bba8ae
fuer <contact@fu-er.com>
2016-04-28 18:48:14 -0700

    Added piece scaling to fit tiles; added basic dragging and snapping

---
+++ Assets/scripts/Board/Dragger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+namespace Board
+{
+  public class Dragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+  {
+    private GameObject currently_dragged;
+    private void Start()
+    {}
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+      GameObject canvas = GameObject.FindGameObjectWithTag("main_canvas");
+      currently_dragged = Instantiate(gameObject);
+      currently_dragged.transform.SetParent(canvas.transform);
+      currently_dragged.GetComponent<RectTransform>().anchoredPosition = eventData.position;
+
+      /* Have to manually set size probably because HorizontalLayoutGroup is messing with it? */
+      currently_dragged.GetComponent<RectTransform>().sizeDelta = GetComponent<RectTransform>().sizeDelta;
+
+      /* To raycast to the tiles under it */
+      currently_dragged.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+      currently_dragged.GetComponent<RectTransform>().anchoredPosition = eventData.position;
+      if(eventData.pointerCurrentRaycast.gameObject != null)
+      {
+        Tile t = eventData.pointerCurrentRaycast.gameObject.GetComponent<Tile>();
+        if(t != null)
+        {
+          currently_dragged.GetComponent<RectTransform>().position = eventData.pointerCurrentRaycast.gameObject.GetComponent<RectTransform>().position;
+        }
+      }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+      currently_dragged.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+      /* Can't really place it for now */
+      Destroy(currently_dragged);
+    }
+  }
+}
---
+++ Assets/scripts/Board/Scaler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+namespace Board
+{
+  public class Scaler : MonoBehaviour
+  {
+    private void Start()
+    {
+      RectTransform tile_rect = GameObject.FindObjectOfType<Tile>().GetComponent<RectTransform>();
+      //Debug.Log("tile size x " + tile_rect.sizeDelta.x + ", my size x " + GetComponent<RectTransform>().sizeDelta.x);
+      float scale_amount = tile_rect.sizeDelta.x / GetComponent<RectTransform>().sizeDelta.x;
+      //Debug.Log("scale amount " + scale_amount);
+      Vector3 scale = transform.parent.localScale;
+      scale *= scale_amount;
+      transform.parent.localScale = scale;
+
+      /* Only scale once! Prevents clones from scaling again and again */
+      Destroy(this);
+    }
+  }
+}
