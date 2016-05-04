31a0f543447cb5735ab9aca16c55a61f7bef942d
jeaye <contact@jeaye.com>
2016-05-03 23:51:29 -0700

    Update logger symbol

--- Assets/scripts/Logger.cs
+++ Assets/scripts/Logger.cs
@@ -3,61 +3,61 @@ using UnityEngine;
 // http://forum.unity3d.com/threads/strip-release-build-from-all-debug-log-calls.353600/#post-2292960
 public sealed class Logger
 {
-  public const string LOGGER_SYMBOL = "ENABLE_LOG";
+  public const string Symbol = "ENABLE_LOG";
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void Log(object message)
   { Debug.Log(message); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void Log(object message, Object context)
   { Debug.Log(message, context); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogFormat(string message, params object[] args)
   { Debug.LogFormat(message, args); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogFormat(Object context, string message, params object[] args)
   { Debug.LogFormat(context, message, args); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogWarning(object message)
   { Debug.LogWarning(message); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogWarning(object message, Object context)
   { Debug.LogWarning(message, context); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogWarningFormat(string message, params object[] args)
   { Debug.LogWarningFormat(message, args); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogWarningFormat(Object context, string message, params object[] args)
   { Debug.LogWarningFormat(context, message, args); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogError(object message)
   { Debug.LogError(message); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogError(object message, Object context)
   { Debug.LogError(message, context); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogErrorFormat(string message, params object[] args)
   { Debug.LogErrorFormat(message, args); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogErrorFormat(Object context, string message, params object[] args)
   { Debug.LogErrorFormat(context, message, args); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogException(System.Exception exception)
   { Debug.LogException(exception); }
 
-  [System.Diagnostics.Conditional(LOGGER_SYMBOL)]
+  [System.Diagnostics.Conditional(Symbol)]
   public static void LogException(System.Exception exception, Object context)
   { Debug.LogException(exception, context); }
 }
