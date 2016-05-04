483049bf6a52f0483b8ea0ad69b7aa69b08496e8
fuer <contact@fu-er.com>
2016-05-04 16:02:41 -0700

    Added score

---
+++ Assets/scripts/Board/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Board
+{
+  [RequireComponent(typeof(Text))]
+  public class Score : MonoBehaviour
+  {
+    private Text text;
+
+    private void Awake()
+    {
+      text = GetComponent<Text>();
+      text.text = "0";
+    }
+  }
+}
