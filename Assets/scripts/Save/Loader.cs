5291dc4634cf621ffac1efba41a330a74c9f3a26
jeaye <contact@jeaye.com>
2016-05-29 11:50:17 -0700

    Remove an old todo

--- Assets/scripts/Save/Loader.cs
+++ Assets/scripts/Save/Loader.cs
@@ -23,7 +23,6 @@ namespace Save
     /* TODO: Save the current piece tray. */
     /* TODO: Save the current score. */
     /* TODO: Save the whole pieces. */
-    /* TODO: Save the score. */
   }
 
   public class Loader : MonoBehaviour
