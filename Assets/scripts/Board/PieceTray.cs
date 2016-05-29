cf849661e962d6ba9c8696eec332d570919992e0
jeaye <contact@jeaye.com>
2016-05-29 14:57:20 -0700

    Remove some old debug code

--- Assets/scripts/Board/PieceTray.cs
+++ Assets/scripts/Board/PieceTray.cs
@@ -81,7 +81,6 @@ namespace Board
       foreach (Transform child in transform)
       { child.Rotate(new Vector3(0, 0, -60)); }
       Notification.Pool.Dispatch(new RotateNeighbours());
-      //Notification.Pool.Dispatch(new PrintDebug());
     }
   }
 }
