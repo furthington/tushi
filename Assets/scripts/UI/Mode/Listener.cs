d1ad772b4c156eb78a6f503b89ff172c62f06aa3
fuer <contact@fu-er.com>
2016-06-03 15:43:09 -0700

    Added lefty mode functionality

---
+++ Assets/scripts/UI/Mode/Listener.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Notification;
+
+namespace UI.Mode
+{
+  public class Read
+  { }
+
+  public class ReadReply
+  {
+    public bool Right
+    { get; set; }
+
+    public ReadReply(bool r)
+    { Right = r; }
+  }
+
+  public class Write
+  {
+    public bool Right
+    { get; set; }
+
+    public Write(bool r)
+    { Right = r; }
+  }
+
+  public class Listener : MonoBehaviour
+  {
+    public List<GameObject> left_ui; /* Assign in editor. */
+    public List<GameObject> right_ui; /* Assign in editor. */
+    private SubscriptionStack subscriptions = new SubscriptionStack();
+
+    private bool is_right = true;
+
+    private void Start()
+    {
+      /* TODO: read saved mode. */
+      SetMode(true);
+
+      subscriptions.Add<Read>(_ => Pool.Dispatch(new ReadReply(is_right)));
+      subscriptions.Add<Write>
+      (
+        mode =>
+        {
+          SetMode(mode.Right);
+          SaveMode();
+        }
+      );
+    }
+
+    private void OnDisable()
+    { subscriptions.Clear(); }
+
+    private void SetMode(bool right)
+    {
+      is_right = right;
+      foreach (GameObject obj in right_ui)
+      { obj.SetActive(right); }
+      foreach (GameObject obj in left_ui)
+      { obj.SetActive(!right); }
+    }
+
+    private void SaveMode()
+    {
+      /* TODO: save current mode. */
+    }
+  }
+}
---
+++ Assets/scripts/UI/Mode/Toggler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Notification;
+
+namespace UI.Mode
+{
+  public class Toggler : MonoBehaviour
+  {
+    public Button left; /* Assign in editor. */
+    public Button right; /* Assign in editor. */
+
+    private SubscriptionStack subscriptions = new SubscriptionStack();
+
+    private void Start()
+    {
+      subscriptions.Add<ReadReply>
+      (
+        r =>
+        {
+          right.gameObject.SetActive(r.Right);
+          left.gameObject.SetActive(!r.Right);
+          subscriptions.Clear();
+        }
+      );
+      Pool.Dispatch(new Read());
+    }
+
+    private void OnDisable()
+    { subscriptions.Clear(); }
+
+    public void SetMode(bool r)
+    { Pool.Dispatch(new Write(r)); }
+  }
+}
