2fa3e9e75103c7fdff2a227807b8d479eaf02d97
fuer <contact@fu-er.com>
2016-06-03 13:07:19 -0700

    Updated hamburger image

--- Assets/scripts/Menu/Populator.cs
+++ Assets/scripts/Menu/Populator.cs
@@ -6,6 +6,13 @@ namespace Menu
 {
   public class Populator : MonoBehaviour
   {
+    public Text score; /* Assign in editor. */
+
+    private void Start()
+    {
+      score.text = GameObject.FindGameObjectWithTag("score")
+                     .GetComponent<Text>().text;
+    }
     public void Close()
     { Destroy(gameObject); }
   }
