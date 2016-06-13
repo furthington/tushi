cf3492915bdd6647ea49022ce86ec3fd6278193b
fuer <contact@fu-er.com>
2016-06-13 16:26:58 -0700

    Added piece proxy

---
+++ Assets/scripts/Board/PieceProxy.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Error;
+
+namespace Board
+{
+  public class PieceProxy : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+  {
+    private int index;
+    private GameObject tray;
+    private Dragger dragger = null;
+
+    private void Start()
+    {
+      index = transform.GetSiblingIndex();
+      tray = GameObject.FindGameObjectWithTag("tray");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+      Transform child = tray.transform.GetChild(index);
+      Assert.Invariant(child != null, "Piece tray has no child of index " + index);
+      dragger = child.GetComponent<Dragger>();
+      Assert.Invariant(dragger != null, "Piece tray's child " + index + " has no dragger!");
+      dragger.OnBeginDrag(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+      if (dragger != null)
+      { dragger.OnDrag(eventData); }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+      if (dragger != null)
+      { dragger.OnEndDrag(eventData); }
+    }
+  }
+}
