03165a76c2ec2eccc63b8b011381f72c1d2f51d4
jeaye <contact@jeaye.com>
2016-06-13 21:20:23 -0700

    Allow back button to show the menu

--- Assets/scripts/Menu/Popper.cs
+++ Assets/scripts/Menu/Popper.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using Notification;
 
 namespace Menu
 {
   public class Popper : MonoBehaviour
   {
     public GameObject menu_prefab; /* Assign in editor. */
+
+    private void Start()
+    { Pool.Dispatch(new UI.AddBackButtonHandler(gameObject, Show)); }
 
     public void Show()
     { Instantiate(menu_prefab); }
