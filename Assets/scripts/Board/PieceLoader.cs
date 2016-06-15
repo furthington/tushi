feefdde7511ce742f8b7773c0cca6df27a22ff5e
jeaye <contact@jeaye.com>
2016-06-14 20:32:00 -0700

    Add a performance timer to piece loading

--- Assets/scripts/Board/PieceLoader.cs
+++ Assets/scripts/Board/PieceLoader.cs
@@ -39,40 +39,43 @@ namespace Board
 
     private void OnLoadPiece(Load l)
     {
-      /* TODO: Cache this? */
-      var canvas = GameObject.FindGameObjectWithTag("main_canvas");
+      using(var timer = new Profile.TaskTimer("Load piece"))
+      {
+        /* TODO: Cache this? */
+        var canvas = GameObject.FindGameObjectWithTag("main_canvas");
 
-      /* TODO: Copied from Row loading */
-      var obj = new GameObject().AddComponent<Piece>();
-      obj.gameObject.AddComponent<CanvasGroup>().blocksRaycasts = false;
-      obj.gameObject.AddComponent<PieceIdentifier>().ID = l.Item.ID;
-      obj.transform.position = new Vector3
-      (
-        l.Item.Position[0],
-        l.Item.Position[1],
-        l.Item.Position[2]
-      );
-      obj.transform.localScale = new Vector3
-      (
-        l.Item.Scale[0],
-        l.Item.Scale[1],
-        l.Item.Scale[2]
-      );
-      obj.transform.rotation = new Quaternion
-      (
-        l.Item.Rotation[0],
-        l.Item.Rotation[1],
-        l.Item.Rotation[2],
-        l.Item.Rotation[3]
-      );
+        /* TODO: Copied from Row loading */
+        var obj = new GameObject().AddComponent<Piece>();
+        obj.gameObject.AddComponent<CanvasGroup>().blocksRaycasts = false;
+        obj.gameObject.AddComponent<PieceIdentifier>().ID = l.Item.ID;
+        obj.transform.position = new Vector3
+        (
+          l.Item.Position[0],
+          l.Item.Position[1],
+          l.Item.Position[2]
+        );
+        obj.transform.localScale = new Vector3
+        (
+          l.Item.Scale[0],
+          l.Item.Scale[1],
+          l.Item.Scale[2]
+        );
+        obj.transform.rotation = new Quaternion
+        (
+          l.Item.Rotation[0],
+          l.Item.Rotation[1],
+          l.Item.Rotation[2],
+          l.Item.Rotation[3]
+        );
 
-      obj.gameObject.AddComponent<Image>();
-      obj.transform.SetParent(canvas.transform);
-      obj.GetComponent<Image>()
-         .sprite = Resources.Load<Sprite>(l.Item.Name);
-      obj.GetComponent<Image>().SetNativeSize();
+        obj.gameObject.AddComponent<Image>();
+        obj.transform.SetParent(canvas.transform);
+        obj.GetComponent<Image>()
+           .sprite = Resources.Load<Sprite>(l.Item.Name);
+        obj.GetComponent<Image>().SetNativeSize();
 
-      Pool.Dispatch(new Loaded(l.Item.ID, obj));
+        Pool.Dispatch(new Loaded(l.Item.ID, obj));
+      }
     }
   }
 }
