1cef7e1e94ca0fd5a59e2dc4f78bf7e4486115a6
fuer <contact@fu-er.com>
2016-06-20 17:17:03 -0700

    Added help screen

---
+++ Assets/scripts/Help/Populator.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using Notification;
+
+namespace Help
+{
+  public class Populator : MonoBehaviour
+  {
+    /* Assign all in editor. */
+    public Image current;
+    public Image next;
+    public AnimationCurve curve;
+    public List<Sprite> sprites;
+    public float transit_time;
+
+    private void Start()
+    {
+      Pool.Dispatch(new UI.AddBackButtonHandler(gameObject, Close));
+      StartCoroutine(Scroll());
+    }
+
+    private IEnumerator Scroll()
+    {
+      /* Image size is still invalid at start, give it one frame to be set. */
+      yield return new WaitForEndOfFrame();
+      float height = current.rectTransform.rect.height;
+      Vector2 origin = current.rectTransform.anchoredPosition;
+      int current_index = Random.Range(0, sprites.Count);
+      int next_index = (Random.Range(1, sprites.Count) + current_index) % sprites.Count;
+      current.sprite = sprites[current_index];
+      next.sprite = sprites[next_index];
+      while (true)
+      {
+        /* Reset image positions. */
+        Vector2 pos = origin;
+        pos.y += height;
+        next.rectTransform.anchoredPosition = pos;
+        current.rectTransform.anchoredPosition = origin;
+        yield return new WaitForSeconds(1.0f);
+
+        /* Play transition. */
+        float start_time = Time.time;
+        float elapsed = 0.0f;
+        while (elapsed < transit_time)
+        {
+          pos.y = origin.y - curve.Evaluate(elapsed / transit_time) * height;
+          current.rectTransform.anchoredPosition = pos;
+          pos.y += height;
+          next.rectTransform.anchoredPosition = pos;
+          yield return null;
+          elapsed = Time.time - start_time;
+        }
+
+        /* Roll next sprite. */
+        current.sprite = next.sprite;
+        next_index = (Random.Range(1, sprites.Count) + next_index) % sprites.Count;
+        next.sprite = sprites[next_index];
+      }
+    }
+
+    public void Close()
+    {
+      Pool.Dispatch(new UI.RemoveBackButtonHandler(gameObject));
+      Destroy(gameObject);
+    }
+  }
+}
