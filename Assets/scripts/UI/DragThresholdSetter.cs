55a9724289cff85219102945463a65a534337495
fuer <contact@fu-er.com>
2016-06-24 15:48:11 -0700

    Added drag threshold setter

---
+++ Assets/scripts/UI/DragThresholdSetter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+  public class DragThresholdSetter : MonoBehaviour
+  {
+    void Start()
+    { EventSystem.current.pixelDragThreshold = 10; }
+
+  }
+}
