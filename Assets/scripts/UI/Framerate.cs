ea92d70ba07cb6b93fe059409ab30cfe77b89509
fuer <contact@fu-er.com>
2016-06-30 14:35:42 -0700

    Added frame rate counter

---
+++ Assets/scripts/UI/Framerate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace UI
+{
+  public class Framerate : MonoBehaviour
+  {
+    public int target_frame_rate;
+    private Text txt = null;
+
+    void Start()
+    {
+      Application.targetFrameRate = target_frame_rate;
+      txt = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+      if (txt == null)
+      { return; }
+      string str = "Target: " + Application.targetFrameRate;
+      str += "\n" + "FPS: " + (1.0f / Time.deltaTime).ToString();
+      txt.text = str;
+    }
+  }
+}
